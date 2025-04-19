5e03c527752c8d09460c32d38660466ac078a5a5
Aalekh Modgil <aalekhmodgil8@gmail.com>
2025-04-19 08:56:59 +0530

    feat: added property model and connected with frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,32 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Property.create!({
+  name: 'Sample Property',
+  description: 'This is description of sample',
+  headline: 'This is headline of sample',
+  address_1: 'This is address_1 of sample',
+  address_2: 'This is address_2 of sample',
+  city: 'City 1',
+  state: 'This is state of sample',
+  country: 'Country 1 '
+})
+
+Property.create!({
+  name: 'Sample Property 2',
+  description: 'This is description of sample 2',
+  headline: 'This is headline of sample 2',
+  address_1: 'This is address_1 of sample 2',
+  address_2: 'This is address_2 of sample 2',
+  city: 'City 2',
+  state: 'This is state of sample 2',
+  country: 'Country 2'
+})
+
+Property.create!({
+  name: 'Sample Property 4',
+  description: 'This is description of sample 4',
+  headline: 'This is headline of sample 4',
+  address_1: 'This is address_1 of sample 4',
+  address_2: 'This is address_2 of sample 4',
+  city: 'City 4',
+  state: 'This is state of sample 4',
+  country: 'Country 4'
+})
